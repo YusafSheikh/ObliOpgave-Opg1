3e5b29540f163c60a7302749c2a3f778a17494e0
yusaf <yusaf@DESKTOP-SCF39CV>
2023-03-07 09:25:23 +0100

    Add project files.

---
+++ ObliOpgave/Car.cs
@@ -0,0 +1,63 @@
+﻿namespace ObliOpgave
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string? Model { get; set; }
+
+        public int Price { get; set; }
+
+        public string? LicensePlate { get; set; }
+
+        public void ValidatePrice()
+        {
+            if (Price < 0)
+            {
+                throw new ArgumentOutOfRangeException("Price must not be below 0: " + Price);
+            }
+
+        }
+
+        public void ValidateId()
+        {
+            if (Id < 1)
+            {
+                throw new ArgumentOutOfRangeException("Id must be over 1" + Id);
+            }
+        }
+
+        public void ValidateModel() 
+        {
+            if (Model == null)
+            {
+                throw new ArgumentNullException("Model is null");
+            }
+
+            if (Model?.Length < 4)
+            {
+                throw new ArgumentOutOfRangeException("Model must be at least 4 characthers");
+            }
+        }
+
+        public void ValidateLicensePlate()
+        {
+            if (LicensePlate?.Length == null)
+            {
+                throw new ArgumentOutOfRangeException("License plate must be at least 2 characthers");
+            }
+            if (LicensePlate?.Length < 2 || LicensePlate?.Length > 7) 
+            {
+                throw new ArgumentOutOfRangeException("License plate must be max 7 characthers");
+            }
+        }
+        public void Validate() 
+        { 
+            ValidatePrice();
+            ValidateId();
+            ValidateModel();
+            ValidateLicensePlate();
+        }  
+
+      
+    }
+}
---
+++ ObliOpgaveTests/CarTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObliOpgave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObliOpgave.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        Car car = new Car() { Id = 2, Model = "Mercedes E Klasse", LicensePlate = "DA23543", Price = 420000 };
+        Car carNegativePrice = new Car() { Id = 5, Model = "Tesla S Model", LicensePlate = "GR32683", Price = -79500 };
+        Car carUnknownModel = new Car() { Id = 53, Model = "C63", LicensePlate = "B1TC01N", Price = 10043950 };
+        Car carUnknownId = new Car() { Id = -34, Model = "Bugatti Chiron", LicensePlate = "T8", Price = 35300000 };
+        Car carUnknownLicensePlate = new Car() { Id = 43, Model = "Audi R8", LicensePlate = "CH435439", Price = 2700000 };
+
+        [TestMethod()]
+        public void ValidatePriceTest()
+        {
+
+            // Act & Assert
+            car.ValidatePrice();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carNegativePrice.ValidatePrice());
+        }
+
+        [TestMethod()]
+        public void ValidateIdTest()
+        {
+            // Act & Assert
+            car.ValidateId();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carUnknownId.ValidateId());
+        }
+
+        [TestMethod()]
+        public void ValidateModelTest()
+        {
+
+            // Act & Assert
+            car.ValidateModel();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carUnknownModel.ValidateModel());
+        }
+
+        [TestMethod()]
+        public void ValidateLicensePlate()
+        {
+
+            car.ValidateLicensePlate();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carUnknownLicensePlate.ValidateLicensePlate());
+        }
+    }
+
+}
